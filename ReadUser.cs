3d3f2051cce77663315edf7551fbc30c96b31aed
MatheusSantos285 <santos0508matheus@outlook.com>
2025-02-16 16:58:46 -0300

    Código do exercício 8

---
+++ ReadUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1_C_
+{
+    internal class ReadUser
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine("Digite o seu nome:");
+            string nome = Console.ReadLine();
+            Console.WriteLine($"Hello, {nome}!");
+        }
+    }
+}
