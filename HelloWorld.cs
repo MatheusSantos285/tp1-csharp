27065b24addcc0da3ca9eb3b168e762b5b796e13
MatheusSantos285 <santos0508matheus@outlook.com>
2025-02-16 15:59:13 -0300

    Código dos exercícios 5, 6

---
+++ HelloWorld.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1_C_
+{
+    internal class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
