86c4c90cfd0c41f85ad6285fa9475294b5b30177
MatheusSantos285 <santos0508matheus@outlook.com>
2025-02-16 17:05:19 -0300

    Código do exercício 9

---
+++ VariablesTypes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1_C_
+{
+    internal class VariablesTypes
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+            string nome;
+            double altura;
+
+            idade = 20;
+            nome = "Matheus";
+            altura = 1.70;
+
+            Console.WriteLine($"Nome: {nome}\nIdade: {idade}\nAltura: {altura}");
+        }
+    }
+}
